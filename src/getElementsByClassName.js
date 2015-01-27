f2b1ddea23207b02bfa2b100aac4b6df14ff4b3a
Pavan Ravipati <poppin3000@gmail.com>
2015-01-27 12:15:19 -0800

    getElementsByClassName works for nested nodes now

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,17 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+
+result = [];
+
+var getElementsByClassName = function(className, currElement
 ){
   // your code here
-  var ob = document.body;
-  result = [];
+  
+  var ob = typeof(currElement) != 'undefined' ? currElement : document.body;
 
   for (var i = 0; i < ob.childNodes.length; i++) {
       if (ob.childNodes[i].classList) {
 	  if (ob.childNodes[i].classList.contains(className)) {
 	      result.push(ob.childNodes[i]);
 	  }
+      }
+      if (ob.childNodes[i].childNodes) {
+	  getElementsByClassName(className, ob.childNodes[i]);
       }
   }
   return result;
