dd5e1365980d00bea72a4796cf2911eaeef7cf12
Pavan Ravipati <poppin3000@gmail.com>
2015-01-27 16:08:35 -0800

    Updated getElementsByClassName. Appears to be working properly but still not passing tests.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,22 +5,22 @@
 
 // But instead we're going to implement it from scratch:
 
-result = [];
+
 
 var getElementsByClassName = function(className, currElement
 ){
   // your code here
-  
-  var ob = typeof(currElement) != 'undefined' ? currElement : document.body;
+  var ob = typeof currElement != 'undefined' ? currElement : document.body;
+  var result = [];
 
   for (var i = 0; i < ob.childNodes.length; i++) {
-      if (ob.childNodes[i].classList) {
+      if (ob.childNodes[i].classList !== undefined) {
 	  if (ob.childNodes[i].classList.contains(className)) {
 	      result.push(ob.childNodes[i]);
 	  }
       }
-      if (ob.childNodes[i].childNodes) {
-	  getElementsByClassName(className, ob.childNodes[i]);
+      if (ob.childNodes[i].childNodes.length > 0) {
+	  result = result.concat(getElementsByClassName(className, ob.childNodes[i]));
       }
   }
   return result;
